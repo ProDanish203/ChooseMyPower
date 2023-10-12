3fafe7b353a871c067d1c2286eeee23bc3350570
Danish <danishsidd524@gmail.com>
2023-10-12 17:30:29 +0500

    Login setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,28 @@
-import { Footer, Header } from '@/components/shared'
-import './globals.css'
+import { Header } from '@/components/shared'
+import '../globals.css'
 import type { Metadata } from 'next'
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
 import { AOS } from '@/components/helpers';
+import { GET } from '../api/auth/[...nextauth]/route';
+import { Provider } from '@/lib/Provider';
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
 export const metadata: Metadata = {
-  title: 'Choose My Power',
+  title: 'Choose My Power | Admin',
   description: 'Your personal reliable power broker',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const session = await getServerSession(GET)
+  // @ts-ignore
+  if(!session?.user) redirect("/login")
 
   return (
     <html lang="en">
@@ -22,8 +30,11 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body >
+
+      <Provider>
+
         <AOS/>
-      <Theme accentColor="blue" grayColor="sand" radius="large" scaling="95%">
+        <Theme accentColor="blue" grayColor="sand" radius="large" scaling="95%">
   
         <div>
           <Header/>
@@ -32,11 +43,9 @@ export default function RootLayout({
         <div className='bg-bg pt-16 min-h-[100vh]'>
           {children}
         </div>
-        
-        <div>
-          <Footer/>
-        </div>
+
       </Theme>
+      </Provider>
       </body>
     </html>
   )
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,43 @@
+import { Footer, Header } from '@/components/shared'
+import '../globals.css'
+import type { Metadata } from 'next'
+import '@radix-ui/themes/styles.css';
+import { Theme } from '@radix-ui/themes';
+import { AOS } from '@/components/helpers';
+
+export const metadata: Metadata = {
+  title: 'Choose My Power',
+  description: 'Your personal reliable power broker',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  return (
+    <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+      </head>
+      <body >
+        <AOS/>
+      <Theme accentColor="blue" grayColor="sand" radius="large" scaling="95%">
+  
+        <div>
+          <Header/>
+        </div>  
+
+        <div className='bg-bg pt-16 min-h-[100vh]'>
+          {children}
+        </div>
+        
+        <div>
+          <Footer/>
+        </div>
+      </Theme>
+      </body>
+    </html>
+  )
+}
