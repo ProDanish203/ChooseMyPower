e489c181b14e1fb0a07d349364ec6b50275570ae
Danish <danishsidd524@gmail.com>
2023-10-15 04:38:18 +0500

    Completed rate details modal

--- components/cards/RateModal.tsx
+++ components/cards/RateModal.tsx
@@ -16,13 +16,12 @@ export const RateModal = ({id, setShowModal}: Props) => {
     const {data, mutate, isLoading, error} = useSWR(`/api/getRatesById/${id}`, fetcher);
     
     // const { plan_id, zip_code, company_unique_id, company_id, company_tdu_id, company_tdu_name, company_name, company_logo, website, plan_name, plan_details, plan_type, special_terms, rate_type, term_value, price_kwh500, price_kwh1000, price_kwh2000, pricing_details, fact_sheet, terms_of_service, go_to_plan, yrac_url, enroll_phone, renewable_energy_id, renewable_energy_description, rating_total , rating_count, jdp_rating, jdp_rating_year, sort_key} = ;
-    console.log(data);
   return (
-    <div className="bg-black modal-bg flex items-center justify-center flex-col fixed top-0 left-0 w-full h-screen z-40">
+    <div className="bg-black modal-bg flex items-center justify-center flex-col fixed top-0 left-0 w-full h-screen z-40 p-3">
 
-        <div className="relative bg-gray-100 shadow-md rounded-md max-w-[750px] w-full mx-auto">
+        <div className="relative bg-gray-100 shadow-md rounded-md max-w-[850px] max-h-[80vh] overflow-hidden overflow-y-auto w-full mx-auto">
 
-            <div className="w-full rounded-md py-2 px-4 flex items-center gap-2 justify-between bg-primary">
+            <div className="w-full sticky top-0 z-20 rounded-md py-2 px-4 flex items-center gap-2 justify-between bg-primary">
                 <p className="text-white text-lg font-bold capitalize">Plan details</p>
                 <i className="fas fa-times text-white text-2xl cursor-pointer" 
                 onClick={() => setShowModal(false)}
@@ -34,9 +33,9 @@ export const RateModal = ({id, setShowModal}: Props) => {
         </div>
         ) : ( 
         <>
-        <div className="py-5 px-4 flex justify-between gap-3 items-center ">
+        <div className="py-5 px-4 flex max-sm:flex-col xs:gap-10 justify-between gap-3 items-center ">
         
-            <div className="flex items-center">
+            <div className="flex max-sm:flex-col max-sm:gap-5 items-center">
                 <div className="relative">
                     <Image src={data?.company_logo} alt={data?.company_name} width={200} height={150}/>
                 </div>
@@ -47,15 +46,20 @@ export const RateModal = ({id, setShowModal}: Props) => {
                 </div>
             </div>
 
-            <div className="flex flex-col items-center gap-2">
-                <button className="bg-primary rounded-full text-lg text-white py-2.5 px-4 flex items-center shadow-sm hover:shadow-md hover:gap-3 gap-2 justify-center">
+            <div className="flex flex-col gap-2 max-sm:w-full">
+                <button className="bg-primary rounded-full max-sm:w-full max-sm:block text-lg text-white py-2.5 px-10 flex items-center shadow-sm hover:shadow-md hover:gap-3 gap-2 justify-center">
                     Select Plan
-                    <i className="fas fa-arrow-right-long"></i>
+                    <i className="fas fa-arrow-right-long max-sm:ml-3"></i>
                 </button>
 
                 <Link href={data?.website} target="_blank" className="flex gap-2 items-center text-primary text-lg">
                     <i className="fa-solid fa-arrow-up-right-from-square"></i>
                     Website
+                </Link>
+
+                <Link href={`tel:${data?.enroll_phone}`} className="flex gap-2 items-center text-primary text-lg">
+                    <i className="fa-solid fa-phone"></i>
+                    {data?.enroll_phone}
                 </Link>
             </div>
 
@@ -63,7 +67,7 @@ export const RateModal = ({id, setShowModal}: Props) => {
 
         <div className="bg-gray-400 w-full h-[2px] mb-3"/>
 
-        <div className="py-1 px-4 flex items-center gap-4">
+        <div className="py-1 px-4 flex max-sm:flex-col max-sm:items-start items-center gap-4">
             <Link href={data?.fact_sheet} target="_blank" className="flex gap-2 items-center text-primary">
                 <i className="far fa-folder text-xl"></i>
                 <p className="font-semibold">Electricity Facts Table</p>
@@ -80,7 +84,7 @@ export const RateModal = ({id, setShowModal}: Props) => {
             </Link>
         </div>
         
-        <div className="py-2 px-4 flex items-center gap-3">
+        <div className="py-2 px-4 flex max-sm:flex-col max-sm:items-start items-center gap-10 pb-10">
             <div className="flex flex-col justify-center gap-1">   
                 <div className="flex items-center gap-1">
                     <p className="py-1.5 px-10 text-center bg-gray-300 text-primary text-lg font-semibold">Rate 500kwh</p>
@@ -96,8 +100,25 @@ export const RateModal = ({id, setShowModal}: Props) => {
                 </div>
             </div>
 
-            <div>
+            <div className="flex flex-col gap-2">
+                <div className="flex items-center max-sm:flex-col max-xs:items-start max-sm:gap-2 gap-10">
+                    <div>
+                        <h4 className="text-4xl font-extrabold text-primary ">{data?.price_kwh500}<i className="fas fa-cent-sign"></i></h4>
+                        <p className="text-gray-600 font-semibold">Per KWH</p>
+                    </div>
 
+                    <div>
+                        <h4 className="text-4xl font-extrabold text-primary ">{data?.term_value} month</h4>
+                        <p className="text-gray-600 font-semibold">{data?.rate_type}</p>
+                    </div>
+                </div>
+
+                <div className="mt-3">
+                    <h4 className="text-primary text font-semibold flex items-center gap-2">
+                        <i className="fas fa-calendar-xmark"></i>
+                        <p>{data?.pricing_details}</p>
+                    </h4>
+                </div>
             </div>
         </div>
         </>
